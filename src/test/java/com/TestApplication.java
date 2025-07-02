0fec685bb1d4bf6d303116ed6a78d5cf9b89c909
bs <26665808+bs-public@users.noreply.github.com>
2025-07-03 09:41:38 +1000

    Spring Boot Test Containers Initial

---
+++ src/test/java/com/TestApplication.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+  }
+}
