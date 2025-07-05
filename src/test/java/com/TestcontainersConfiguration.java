97d5b5b3dfd7b231fe6844bcf5291c61b5c4baff
bs <26665808+bs-public@users.noreply.github.com>
2025-07-05 10:00:45 +1000

    Add bean for Testcontainer - MySQLContainer

--- src/test/java/com/TestcontainersConfiguration.java
+++ src/test/java/com/TestcontainersConfiguration.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+/**
+ * @TestConfiguration which defines beans for the Testcontainers
+ */
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
   @Bean
   @ServiceConnection
   MySQLContainer<?> mysqlContainer() {
-    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    return new MySQLContainer<>(DockerImageName.parse("mysql:8.4.5"))
+        .withDatabaseName("my_db")
+        .withUsername("my_user")
+        .withPassword("my_password");
   }
 }
